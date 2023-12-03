319d43ddf01be1648357160e039d2fe34aca7cdc
ccordoba464 <93160733+ccordoba464@users.noreply.github.com>
2023-12-03 12:24:01 -0500

    Add switches to settings page

--- src/App.jsx
+++ src/App.jsx
@@ -1,102 +1,39 @@
 import "./App.css";
 import settings from "../public/icons/gear-solid.svg";
 import cloud from "../public/icons/cloud-solid.svg";
-import { Routes, Route, Link } from "react-router-dom";
+
+import { useState } from "react";
 
 import Dashboard from "./components/Dashboard/Dashboard.jsx";
 import Settings from "./components/Settings/Settings.jsx";
 
 function App() {
+  const [currentView, setCurrentView] = useState("dashboard");
+
+  // Function to change view
+  const changeView = viewName => {
+    setCurrentView(viewName);
+  };
+
   return (
-    <>
-      <div className="extensionContainer">
-        <header className="header">
-          <Link to="/" className="websiteTitle">
-            <img src={cloud} alt="" className="headerIcon" />
-            <h1 className="h1">Z³-Wellness</h1>
-          </Link>
+    <div className="extensionContainer">
+      <header className="header">
+        <div className="websiteTitle" onClick={() => changeView("dashboard")}>
+          <img src={cloud} alt="" className="headerIcon" />
+          <h1 className="h1">Z³-Wellness</h1>
+        </div>
 
-          <Link to="/settings">
-            <img src={settings} alt="" className="headerIcon" />
-          </Link>
-        </header>
+        <img
+          src={settings}
+          alt=""
+          className="headerIcon"
+          onClick={() => changeView("settings")}
+        />
+      </header>
 
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route />
-        </Routes>
-        {/* <div className="logBtnContainer">
-          <div className="logBtn">
-            <h2 className="h2">Going to sleep?</h2>
-            <input
-              type="datetime-local"
-              id="wake-time"
-              name="wake-time"
-            ></input>
-          </div>
-          <div className="logBtn">
-            <h2 className="h2">Waking up?</h2>
-
-            <input
-              type="datetime-local"
-              id="wake-time"
-              name="wake-time"
-            ></input>
-          </div>
-        </div> */}
-        {/* <div className="circleMenu">
-          <CircleView>
-            <CircleList>
-              <li>
-                <HomeIcon
-                  spanID={"sleepIcon_h"}
-                  iconClass={"iconImages_h sleepIconImg"}
-                  iconInfo={"Log Your Sleep"}
-                />
-              </li>
-              <li>
-                <a>
-                  <HomeIcon
-                    spanID={"reportIcon_h"}
-                    iconClass={"iconImages_h reportIconImg"}
-                    iconInfo={"Personal Report"}
-                  />
-                </a>
-              </li>
-              <li>
-                <HomeIcon
-                  spanID={"logIcon_h"}
-                  iconClass={"iconImages_h logIconImg_h"}
-                  iconInfo={"Log Your Exercise, Caffeine, Stress, Water & Mood"}
-                />
-              </li>
-              <li>
-                <HomeIcon
-                  spanID={"mindIcon_h"}
-                  iconClass={"iconImages_h mindIconImg_h"}
-                  iconInfo={"Mindfulness Modules"}
-                />
-              </li>
-              <li>
-                <HomeIcon
-                  spanID={"perIcon"}
-                  iconClass={"iconImages_h perIconImg"}
-                  iconInfo={"Personality Test"}
-                />
-              </li>
-              <li className="noevents">
-                <HomeIcon
-                  spanID={"bedIcon"}
-                  iconClass={"iconImages_h bedIconImg"}
-                  iconInfo={"Your Bedtime Routine"}
-                />
-              </li>
-            </CircleList>
-          </CircleView>
-        </div> */}
-      </div>
-    </>
+      {currentView === "dashboard" && <Dashboard />}
+      {currentView === "settings" && <Settings />}
+    </div>
   );
 }
 
